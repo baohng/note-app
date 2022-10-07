d99cf044bea8fb98252318c02c96b10db9496acf
Eugen <kirishima219@gmail.com>
2022-10-07 14:28:06 +0700

    create project

---
+++ app/src/main/java/ute/example/baohng/MainActivity.java
@@ -0,0 +1,14 @@
+package ute.example.baohng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
