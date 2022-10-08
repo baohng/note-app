e519bc10433cd697569d50a00e448401323c47f9
Eugen <kirishima219@gmail.com>
2022-10-08 22:27:29 +0700

    Create and save note in database, not display on screen yet

--- app/src/main/java/ute/example/baohng/activities/CreateNodeActivity.java
+++ app/src/main/java/ute/example/baohng/activities/CreateNodeActivity.java
@@ -2,14 +2,28 @@ package ute.example.baohng.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import ute.example.baohng.R;
+import ute.example.baohng.database.NoteDatabase;
+import ute.example.baohng.entities.Note;
 
 public class CreateNodeActivity extends AppCompatActivity {
 
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +32,62 @@ public class CreateNodeActivity extends AppCompatActivity {
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                onBackPressed();
+            public void onClick(View view) {onBackPressed();
             }
         });
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+        textDateTime = findViewById(R.id.textDateTime);
+
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote(){
+        if (inputNoteTitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty()
+                && inputNoteText.getText().toString().trim().isEmpty()){
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NoteDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void unused) {
+                super.onPostExecute(unused);
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+
+        }
+        new SaveNoteTask().execute();
+
     }
 }
--- app/src/main/java/ute/example/baohng/activities/HomePage.java
+++ app/src/main/java/ute/example/baohng/activities/HomePage.java
@@ -3,11 +3,17 @@ package ute.example.baohng.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.List;
+
 import ute.example.baohng.R;
+import ute.example.baohng.database.NoteDatabase;
+import ute.example.baohng.entities.Note;
 
 public class HomePage extends AppCompatActivity {
 
@@ -23,5 +29,28 @@ public class HomePage extends AppCompatActivity {
                 startActivity(new Intent(HomePage.this, CreateNodeActivity.class));
             }
         });
+
+        getNotes();
+    }
+
+    private void getNotes(){
+
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>>{
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NoteDatabase
+                        .getDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+
+        }
+
+        new GetNoteTask().execute();
     }
 }
