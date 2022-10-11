d1ef22c0ac3c50d1c0b390123cb117a55272f1d3
Eugen <kirishima219@gmail.com>
2022-10-11 11:35:20 +0700

    login feature UI added

---
+++ app/src/main/java/ute/example/baohng/activities/Login.java
@@ -0,0 +1,15 @@
+package ute.example.baohng.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ute.example.baohng.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
