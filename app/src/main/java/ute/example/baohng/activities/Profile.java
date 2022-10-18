500c4b13dba22d7303d30da1f227a46f1e11c3c5
Eugen <kirishima219@gmail.com>
2022-10-18 21:38:49 +0700

    profile added

---
+++ app/src/main/java/ute/example/baohng/activities/Profile.java
@@ -0,0 +1,16 @@
+package ute.example.baohng.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ute.example.baohng.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
